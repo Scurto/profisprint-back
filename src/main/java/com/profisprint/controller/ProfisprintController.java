fd0d7db3dd0ab54774a3c0cff9cd5e0ed540852d
scurto <1234567890>
2021-12-02 13:22:53 +0200

    test_new

--- src/main/java/com/profisprint/controller/ProfisprintController.java
+++ src/main/java/com/profisprint/controller/ProfisprintController.java
@@ -53,7 +53,6 @@ public class ProfisprintController {
 
         String lastUsedVideos = taskService.findLastUsedVideos(request.getTaskId());
         //        List<String> videos = youtubeService.getVideosByChannelIdAndCount(request.getChannelId(), request.getCountOfVideo());
-
         List<String> videos = youtubeService.getVideosByChannelIdAndCountWithFilterByLastUsedVideo(request.getChannelId(), request.getCountOfVideo(), lastUsedVideos);
         List<AdvertiseDto> advertise = advertiseService.getAdvertisesForSimpleTask(request.getTaskId(), request.getCustomerId(), request.getCountOfAdvertise(), request.getCountOfAdvertiseMove());
 
